af29e0a1a230813e0d19b2560676fdf2ce8fe2ff
John Botero <jhondark@gmail.com>
2020-11-01 23:21:32 -0500

    added header component

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,40 @@
+import styles from "@/components/styles/header.module.scss"
+import Link from "next/link"
+
 function Header() {
-    return <h1>Header</h1>
+    return (
+        <>
+            <header className={styles.header}>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link href="#sobremi">
+                                <a>Sobre mí</a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="#proyectos">
+                                <a>Proyectos</a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="#experiencia">
+                                <a>Experiencia</a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="#contacto">
+                                <a>Contacto</a>
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+                <Link href="#">
+                    <a className={styles.header__cv}>Descargar CV</a>
+                </Link>
+            </header>
+        </>
+    )
 }
 
 export default Header
