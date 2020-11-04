7315fe2863fa759a74917e1fd206e07ae2a69aec
John Botero <jhondark@gmail.com>
2020-11-03 23:17:44 -0500

    added hero component

---
+++ components/Hero.jsx
@@ -0,0 +1,25 @@
+import styles from "./styles/hero.module.scss"
+
+function Hero() {
+    return (
+        <>
+            <article className={styles.hero}>
+                <section className={styles.hero__titles}>
+                    <p>
+                        <span>Hola</span>, soy John Botero
+                    </p>
+                    <h1>Front-End Developer</h1>
+                    <h2>
+                        Desarrollador web / React / Wordpress / Entusiasta
+                        GameDev
+                    </h2>
+                </section>
+                <section className={styles.hero__img}>
+                    <img src="/images/hero-img2.png" alt="hero-img2" />
+                </section>
+            </article>
+        </>
+    )
+}
+
+export default Hero
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,13 @@
 import Header from "@/components/Header"
+import Hero from "@/components/Hero"
 
 function HomePage() {
-    return <Header />
+    return (
+        <>
+            <Header />
+            <Hero />
+        </>
+    )
 }
 
 export default HomePage
