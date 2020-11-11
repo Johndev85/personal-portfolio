dc2e82763ac294084d8e3ac1ee7bbe933fb652a0
John Botero <jhondark@gmail.com>
2020-11-10 19:57:52 -0500

    update GA tag

---
+++ pages/_document.jsx
@@ -0,0 +1,30 @@
+import Document, { Html, Head, Main, NextScript } from "next/document"
+
+export default class MyDocument extends Document {
+    render() {
+        return (
+            <Html lang="es">
+                <Head>
+                    <script
+                        async
+                        src="https://www.googletagmanager.com/gtag/js?id=G-3RBK30WEKY"
+                    ></script>
+                    <script
+                        async
+                        dangerouslySetInnerHTML={{
+                            __html: `window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', 'G-3RBK30WEKY);`,
+                        }}
+                    />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
